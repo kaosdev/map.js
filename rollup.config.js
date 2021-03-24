c68c824f25b3c92eaf9c1a95db79f982b392fac3
elialotti.dev <elialotti.dev@gmail.com>
2021-03-24 19:47:33 +0100

    build: change build process
    build now emits an unbundled version under "lib" folder,
    a bundled esm version under "esm" folder,
    a bundled esm and minifiedd version under "esm" folder,
    css styles under "css" folder,
    and scss styles are kept on the root under "scss" folder

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,24 @@
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import cleanup from "rollup-plugin-cleanup";
-import copy from "rollup-plugin-copy";
-import scss from "rollup-plugin-scss";
 import { terser } from "rollup-plugin-terser";
-
-const sassPlugin = scss({
-  output: "dist/css/style.css",
-  failOnError: true,
-  sass: require("sass"),
-  outputStyle: "compressed",
-});
-
-const copySass = copy({
-  targets: [{ src: "src/styles/**/*", dest: "dist/scss" }],
-});
 
 export default [
   {
     input: "src/index.ts",
     output: {
-      file: "dist/map.js",
+      file: "esm/map.js",
       format: "esm",
     },
-    plugins: [sassPlugin, copySass, nodeResolve(), typescript()],
+    plugins: [nodeResolve(), typescript()],
   },
   {
     input: "src/index.ts",
     output: {
-      file: "dist/map.min.js",
+      file: "esm/map.min.js",
       format: "esm",
     },
     plugins: [
-      sassPlugin,
-      copySass,
       nodeResolve(),
       typescript(),
       terser(),
